5e74f08d181de33a96eb4e6060ed1ac170c199c2
Jonathan Robertson <jrobertson@insitesoft.com>
2022-07-06 10:55:00 -0500

    Auto create app_log.

--- Entrypoint.ps1
+++ Entrypoint.ps1
@@ -1,3 +1,7 @@
 & "$PSScriptRoot\SetPermissions.ps1"
+$appLogPath = "C:\inetpub\wwwroot\App_Log"
+if (-Not (Test-Path $appLogPath)) {
+    New-Item -Type Directory $appLogPath
+}
 
 C:\ServiceMonitor.exe w3svc
